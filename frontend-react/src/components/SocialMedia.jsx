234d48c2d0d6b18aa1bd242a0ef8b86a7b2e15ae
Michelle Watson <michellejwatson1@gmail.com>
2023-11-12 14:05:48 -0800

    functioning download resume button

--- frontend-react/src/components/SocialMedia.jsx
+++ frontend-react/src/components/SocialMedia.jsx
@@ -3,13 +3,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { FaEnvelope } from 'react-icons/fa';
 import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import MichelleWatsonResume from './MichelleWatsonResume.pdf';
 
 const SocialMedia = () => (
   <div className="app__social">
     <a
-      href="/frontend-react/src/components/MichelleWatsonResume.pdf"
-      download="MichelleWatsonResume.pdf"
-      title="MichelleWatsonResume.pdf"
+      href={MichelleWatsonResume}
+      download="MichelleWatsonResume"
+      target="_blank"
+      rel="noreferrer"
     >
       <FontAwesomeIcon icon={faDownload} />
     </a>
