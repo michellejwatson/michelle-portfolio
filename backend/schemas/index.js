f5bbfba315dc65815935b250773e0155a76e3531
Michelle Watson <michellejwatson1@gmail.com>
2023-11-12 18:11:23 -0800

    readd skills schema

--- backend/schemas/index.js
+++ backend/schemas/index.js
@@ -1,5 +1,6 @@
 import projects from './projects'
 import workexperience from './workexperience'
 import contactMe from './contactMe'
+import skills from './skills'
 
-export const schemaTypes = [projects, workexperience, contactMe]
+export const schemaTypes = [projects, workexperience, contactMe, skills]
