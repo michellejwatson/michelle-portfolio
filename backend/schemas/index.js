5f2173f43d29ca433b6413a1cb55a5c3962200aa
Michelle Watson <michellejwatson1@gmail.com>
2023-11-12 18:04:53 -0800

    remove unused backend schemas

--- backend/schemas/index.js
+++ backend/schemas/index.js
@@ -1,8 +1,5 @@
 import projects from './projects'
 import workexperience from './workexperience'
-import skills from './skills'
-import contact from './contact'
 import contactMe from './contactMe'
-import about from './about'
 
-export const schemaTypes = [projects, workexperience, skills, contact, contactMe, about]
+export const schemaTypes = [projects, workexperience, contactMe]
